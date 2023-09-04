a59f7b093c3f322a23fc02e3f67b6b27eedb0ee1
ARSAN <chris@arsancg.com>
2023-09-03 22:05:37 -0600

    name change

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,10 +3,10 @@ import { Icon } from "@iconify/react"
 const Footer = () => {
     return (
         <footer className="flex flex-col items-center py-2 mt-auto font-text">
-            <p className="text-lg">Developed by <span className="font-bold">Matias Atzori</span></p>
+            <p className="text-lg">Developed by <span className="font-bold">C. Celaya</span></p>
             <div className="flex justify-center gap-2 text-6xl">
-                <a href="https://github.com/KingMacking/passwordgenerator"><Icon icon="mdi:github" /></a>
-                <a href="https://www.linkedin.com/in/matiasatzori/"><Icon icon="mdi:linkedin" /></a>
+                <a href="https://github.com/while-basic/passwordgenerator"><Icon icon="mdi:github" /></a>
+                <a href="https://www.linkedin.com/in/christophercelaya/"><Icon icon="mdi:linkedin" /></a>
             </div>
         </footer>
     )
